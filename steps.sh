c7f8e31466488992ea0bf030db066cb4ff3bfa4c
samwhelp <samwhelp@users.noreply.github.com>
2024-11-25 18:54:26 +0800

    fix

--- steps.sh
+++ steps.sh
@@ -793,22 +793,29 @@ gxde_build_os_bootstrap () {
 
 		build_suite="unstable"
 
-		#util_error_echo
-		#util_error_echo debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --include="${build_include}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
-		#util_error_echo
-		#debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --include="${build_include}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
+		util_error_echo
+		util_error_echo debootstrap --no-check-gpg --include="${build_include}" --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
+		util_error_echo
+		debootstrap --no-check-gpg --include="${build_include}" --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
 
-		util_error_echo
-		util_error_echo debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
-		util_error_echo
-		debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
+
+		#util_error_echo
+		#util_error_echo debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
+		#util_error_echo
+		#debootstrap --no-check-gpg --keyring="${build_keyring_for_loong64}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url_for_loong64}"
 
 	else
 
 		util_error_echo
-		util_error_echo debootstrap --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
+		util_error_echo debootstrap --include="${build_include}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
 		util_error_echo
-		debootstrap --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
+		debootstrap --include="${build_include}" --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
+
+
+		#util_error_echo
+		#util_error_echo debootstrap --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
+		#util_error_echo
+		#debootstrap --arch="${build_arch}" "${build_suite}" "${rootfs}" "${package_repo_url}"
 
 	fi
 
